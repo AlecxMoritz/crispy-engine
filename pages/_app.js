ac282e49a3c5047afe52b4ac57db05e9f0a5efe6
AlecxMoritz <alecxmoritz7@gmail.com>
2022-02-21 22:34:03 -0500

    remove MUI & clean up test CSS

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,8 @@
 import '../styles/globals.css';
-import { ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from '../styles/theme';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
       <Component {...pageProps} />
-    </ThemeProvider>
   )
 }
 
